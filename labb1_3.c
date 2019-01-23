b5116f905f6c115fc6038d07ce64b4228aa2c069
UnknownPlayer99 <33175352+UnknownPlayer99@users.noreply.github.com>
2019-01-23 12:11:13 +0100

    labb1_3 Fixed

--- labb1_3.c
+++ labb1_3.c
@@ -2,14 +2,14 @@
 
 int main(){
     float price = 0.0;
-    int vat = 0;
+    int vat = 0.2;
     printf("Product price (VAT included): ");
     scanf(" %f",&price);
     printf("VAT in precent: ");
     scanf(" %d",&vat);
-    float priceNoVat= (float) price/((100.0+vat)/100.0);
+    float priceNoVat = (float) price/((100.0+vat)/100.0);
     float priceVat = (float) price-priceNoVat;
-    printf("Product price VAT excluded: %f\n", priceNoVat);
-    printf("VAT: %f\n", priceVat);
+    printf("Product price VAT excluded: %.2f\n", priceNoVat);
+    printf("VAT: %.2f\n", priceVat);
     return 0;
 }
